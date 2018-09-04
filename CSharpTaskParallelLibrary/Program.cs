558910507220a8f1bf2ed0fa04613d9d4929ac96
Shibu Tamang <lamashibu19@gmail.com>
2018-09-04 20:39:43 +0545

    code executed parallelly utilizing all the processor core equally

--- CSharpTaskParallelLibrary/Program.cs
+++ CSharpTaskParallelLibrary/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpTaskParallelLibrary
@@ -10,6 +11,35 @@ namespace CSharpTaskParallelLibrary
     {
         static void Main(string[] args)
         {
+            //using Task Parallel library
+            //Task 1
+            var t1 = new Task(() => DoTask(1, 1000));
+            t1.Start();
+
+            //Task 2
+            var t2 = new Task(() => DoTask(2, 1000));
+            t2.Start();
+
+            //using long running task which utilize all the processor core
+            Parallel.For(0, 100, x => LongRunningTask());
+
+            Console.ReadKey();
+        }
+
+        public static void DoTask(int id, int sleep)
+        {
+            Console.WriteLine("Task {0} is begining...", id);
+            Thread.Sleep(sleep);
+            Console.WriteLine("Task {0} is completed", id);
+        }
+
+        public static void LongRunningTask()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"Iteration Count: {i}");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
